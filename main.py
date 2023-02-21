28b788505503ce06d47f588257e87f5307ba690b
aaFurze <95361782+aaFurze@users.noreply.github.com>
2023-02-21 11:03:32 +0000

    - Created prims.py  - Implemented PrimsGenerationAlgorithm

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from src.base_maze import Maze
 from src.generation.eller import EllersGenerationAlgorithm
 from src.generation.kruskals import KruskalsGenerationAlgorithm
+from src.generation.prims import PrimsGenerationAlgorithm
 from src.generation.recursive_backtracker import RecursiveBacktrackerAlgorithm
 from src.graphics.draw_maze import GraphicsApp
 from src.solving.flood_fill import FloodFillSolutionCheck
 
 
 def run():
-    test_maze = Maze(64, 36)
+    test_maze = Maze(32, 32)
     # EllersGenerationAlgorithm(test_maze).generate_maze()
-    KruskalsGenerationAlgorithm(test_maze).generate_maze()
+    # KruskalsGenerationAlgorithm(test_maze).generate_maze()
+    PrimsGenerationAlgorithm(test_maze).generate_maze()
     # RecursiveBacktrackerAlgorithm(test_maze).generate_maze()
     # print(len(FloodFillSolutionCheck(test_maze).solve_maze()))
 
