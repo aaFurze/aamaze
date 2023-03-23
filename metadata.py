ebddeb89941edb4c9f80acb135b4117409efab92
aaFurze <aafurze@gmail.com>
2023-03-23 09:21:24 +0000

    - Created metadata.py - Extracted setup.py field info into metadata.py

---
+++ metadata.py
@@ -0,0 +1,10 @@
+
+NAME = "aamaze"
+
+
+VERSION = "0.0.7"
+SHORT_DESCRIPTION = "A python library for generating, solving and displaying mazes."
+PACKAGES = ["", "generation", "graphics", "solving"]
+PACKAGE_DIRECTORY = {"": "aamaze"}
+PYTHON_REQUIRES = ">=3.7"
+SOURCE_URL = "https://github.com/aaFurze/aamaze"
--- setup.py
+++ setup.py
@@ -2,21 +2,24 @@ import os
 
 from setuptools import setup
 
+from metadata import (NAME, PACKAGE_DIRECTORY, PACKAGES, PYTHON_REQUIRES,
+                      SHORT_DESCRIPTION, SOURCE_URL, VERSION)
+
 lib_folder = os.path.dirname(os.path.realpath(__file__))
-requirement_path = lib_folder + '/requirements.txt'
+requirement_path = lib_folder + "/requirements.txt"
+
 install_requires = []
 if os.path.isfile(requirement_path):
     with open(requirement_path) as f:
         install_requires = f.read().splitlines()
 
 setup(
-    name="aamaze",
-    version="0.0.5",
-    description="A python library for generating, solving and displaying mazes.",
+    name=NAME,
+    version=VERSION,
+    description=SHORT_DESCRIPTION,
     install_requires=install_requires,
-    package_dir={"": "aamaze"},
-    packages=["", "generation", "graphics", "solving"],
-    python_requires='>=3.7',
-    url="https://github.com/aaFurze/aamaze",
-
+    package_dir=PACKAGE_DIRECTORY,
+    packages=PACKAGES,
+    python_requires=PYTHON_REQUIRES,
+    url=SOURCE_URL,
 )
