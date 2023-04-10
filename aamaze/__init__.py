27d312f14790f60b807d9c54e7c0f4cf1f851db3
aaFurze <aafurze@gmail.com>
2023-04-10 08:48:51 +0100

    Added pygame message suppress to root __init__.py

--- aamaze/__init__.py
+++ aamaze/__init__.py
@@ -1,2 +1,6 @@
+from os import environ
+
 from .base_maze import GenerationAlgorithm, Maze, MazeNode, SolvingAlgorithm
 from .graphics import GraphicsApp
+
+environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
